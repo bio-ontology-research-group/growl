5bc631d7298289eb950372347fb7cac0989eb207
Robert Hoehndorf <leechuck@leechuck.de>
2015-12-17 10:22:05 +0300

    .

---
+++ OWLBuilder.groovy
@@ -0,0 +1,41 @@
+package groovy.owl
+
+@Grapes([
+          @Grab(group='org.semanticweb.elk', module='elk-owlapi', version='0.4.2'),
+          @Grab(group='net.sourceforge.owlapi', module='owlapi-api', version='4.1.0'),
+          @Grab(group='net.sourceforge.owlapi', module='owlapi-apibinding', version='4.1.0'),
+          @Grab(group='net.sourceforge.owlapi', module='owlapi-impl', version='4.1.0'),
+          @Grab(group='net.sourceforge.owlapi', module='owlapi-parsers', version='4.1.0'),
+          @GrabConfig(systemClassLoader=true)
+        ])
+
+import org.semanticweb.owlapi.model.parameters.*
+import org.semanticweb.elk.owlapi.ElkReasonerFactory;
+import org.semanticweb.elk.owlapi.ElkReasonerConfiguration
+import org.semanticweb.elk.reasoner.config.*
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.reasoner.*
+import org.semanticweb.owlapi.reasoner.structural.StructuralReasoner
+import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.io.*;
+import org.semanticweb.owlapi.owllink.*;
+import org.semanticweb.owlapi.util.*;
+import org.semanticweb.owlapi.search.*;
+import org.semanticweb.owlapi.manchestersyntax.renderer.*;
+import org.semanticweb.owlapi.reasoner.structural.*
+
+class OWLBuilder extends BuilderSupport {
+  def defaultNamespace
+  def ns = [:]
+
+  OWLBuilder() {
+    
+  }
+  OWLBuilder(OWLOntology o) {
+    
+  }
+
+  
+  
+}
