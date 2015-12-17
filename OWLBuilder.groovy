2dead4feaf6209db8def850ab8eff23f17fbd1c6
Robert Hoehndorf <leechuck@leechuck.de>
2015-12-17 10:34:06 +0300

    some basics

--- OWLBuilder.groovy
+++ OWLBuilder.groovy
@@ -25,17 +25,83 @@ import org.semanticweb.owlapi.search.*;
 import org.semanticweb.owlapi.manchestersyntax.renderer.*;
 import org.semanticweb.owlapi.reasoner.structural.*
 
-class OWLBuilder extends BuilderSupport {
+
+// ont.getClassesInSignature(true).each {
+//   def aa = it.toString()
+//   aa = formatClassNames(aa)
+//   if (id2class[aa] != null) {
+//   } else {
+//     id2class[aa] = it
+//   }
+// }
+// def id2name = [:]
+// ont.getClassesInSignature(true).each { cl ->
+//   EntitySearcher.getAnnotationObjects(cl, ont, fac.getRDFSLabel()).each { lab ->
+//     id2name[cl] = lab.getValue().asLiteral()
+//   }
+// }
+// def addAnno = {resource, prop, cont ->
+//   //  OWLAnnotation anno = fac.getOWLAnnotation(fac.getOWLAnnotationProperty(prop.getIRI()), fac.getOWLLiteral(cont))                                                                    
+//   def axiom = fac.getOWLAnnotationAssertionAxiom(fac.getOWLAnnotationProperty(prop.getIRI()), resource.getIRI(), cont)
+//   manager.addAxiom(outont,axiom)
+// }
+
+// def R = { String s ->
+//   if (s == "part-of") {
+//     fac.getOWLObjectProperty(IRI.create("http://purl.obolibrary.org/obo/BFO_0000050"))
+//   } else if (s == "has-part") {
+//     fac.getOWLObjectProperty(IRI.create("http://purl.obolibrary.org/obo/BFO_0000051"))
+//   } else {
+//     fac.getOWLObjectProperty(IRI.create("http://aber-owl.net/#"+s))
+//   }
+// }
+
+// def C = { String s ->
+//   fac.getOWLClass(IRI.create(onturi+s))
+// }
+
+// def and = { cl1, cl2 ->
+//   fac.getOWLObjectIntersectionOf(cl1,cl2)
+// }
+// def some = { r, cl ->
+//   fac.getOWLObjectSomeValuesFrom(r,cl)
+// }
+// def equiv = { cl1, cl2 ->
+//   fac.getOWLEquivalentClassesAxiom(cl1, cl2)
+// }
+// def subclass = { cl1, cl2 ->
+//   fac.getOWLSubClassOfAxiom(cl1, cl2)
+// }
+
+
+
+
+class OWLBuilder {
   def defaultNamespace
   def ns = [:]
+  OWLOntology o 
+  OWLOntologyManager manager
+  OWLDataFactory fac
+  OWLReasoner reasoner
 
-  OWLBuilder() {
-    
+  void initReasoner() {
+    ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor()
+    OWLReasonerConfiguration config = new SimpleConfiguration(progressMonitor)
+    ElkReasonerFactory f1 = new ElkReasonerFactory()
+    this.reasoner = f1.createReasoner(ont,config)
   }
-  OWLBuilder(OWLOntology o) {
-    
-  }
-
   
+  OWLBuilder() {
+    this.manager = OWLManager.createOWLOntologyManager()
+    this.fac = this.manager.getOWLDataFactory()
+    this.o = this.manager.createOntology(IRI.create("http://aber-owl.net/test.owl"))
+    initReasoner()
+  }
+
+  OWLBuilder(OWLOntology o) {
+    this()
+    this.o = o
+    initReasoner()
+  }
   
 }
