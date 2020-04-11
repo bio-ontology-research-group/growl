6ba48aa6a6a2e2c52197aa1f9fcdaa910bf88b07
Robert Hoehndorf <robert.hoehndorf@kaust.edu.sa>
2020-04-11 11:31:12 +0300

    initial pattern-based QT ontology

--- ReadCMO.groovy
+++ ReadCMO.groovy
@@ -24,7 +24,7 @@ import org.semanticweb.owlapi.util.*
 import org.semanticweb.owlapi.search.*
 import org.semanticweb.owlapi.manchestersyntax.renderer.*
 import org.semanticweb.owlapi.reasoner.structural.*
-
+import java.util.regex.Pattern
 
 def and = { cl1, cl2 ->
     fac.getOWLObjectIntersectionOf(cl1,cl2)
@@ -38,19 +38,28 @@ def equiv = { cl1, cl2 ->
 def subclass = { cl1, cl2 ->
     fac.getOWLSubClassOfAxiom(cl1, cl2)
 }
-def R = { String s ->
+def r = { String s ->
     if (s == "part-of") {
 	fac.getOWLObjectProperty(IRI.create("http://purl.obolibrary.org/obo/BFO_0000050"))
     } else if (s == "has-part") {
 	fac.getOWLObjectProperty(IRI.create("http://purl.obolibrary.org/obo/BFO_0000051"))
+    } else if (s == "inheres-in-part-of") {
+	fac.getOWLObjectProperty(IRI.create("http://purl.obolibrary.org/obo/RO_0002314"))
+    } else if (s == "inheres-in") {
+	fac.getOWLObjectProperty(IRI.create("http://purl.obolibrary.org/obo/RO_0000052"))
     } else {
 	fac.getOWLObjectProperty(IRI.create("http://aber-owl.net/#"+s))
     }
 }
-def C = { String s ->
-    fac.getOWLClass(IRI.create(onturi+s))
+def c = { String s ->
+    if (s == "quality") {
+	fac.getOWLClass(IRI.create("http://purl.obolibrary.org/obo/PATO_0000001"))
+    } else {
+	fac.getOWLClass(IRI.create(onturi+s))
+    }
 }
 //def add = { OWLOntology o, OWLAxiom a -> this.manager.addAxiom(o, a) }
+
 
 def labelMap = { ont ->
     def map = [:]
@@ -67,16 +76,22 @@ def labelMap = { ont ->
 }
 
 def manager = OWLManager.createOWLOntologyManager()
-def fac = manager.getOWLDataFactory()
+fac = manager.getOWLDataFactory()
+
+def ont = manager.createOntology(IRI.create("http://biohackathon.org/covid19/cmo-ext.owl"))
 
 def cmo = manager.loadOntologyFromOntologyDocument(new File("cmo.owl"))
 def uberon = manager.loadOntologyFromOntologyDocument(new File("uberon.owl"))
 def chebi = manager.loadOntologyFromOntologyDocument(new File("chebi.owl"))
 def pato = manager.loadOntologyFromOntologyDocument(new File("pato.owl"))
-ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor()
-OWLReasonerConfiguration config = new SimpleConfiguration(progressMonitor)
-ElkReasonerFactory f1 = new ElkReasonerFactory()
-def reasoner = f1.createReasoner(cmo,config)
+// ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor()
+// OWLReasonerConfiguration config = new SimpleConfiguration(progressMonitor)
+// ElkReasonerFactory f1 = new ElkReasonerFactory()
+// def cmoreasoner = f1.createReasoner(cmo,config)
+// def uberonreasoner = f1.createReasoner(uberon,config)
+// def chebireasoner = f1.createReasoner(chebi,config)
+// def patoreasoner = f1.createReasoner(pato,config)
+
 
 def cmolabels = labelMap(cmo)
 def uberonlabels = labelMap(uberon)
@@ -84,21 +99,64 @@ def chebilabels = labelMap(chebi)
 def patolabels = labelMap(pato)
 
 def cmo2other = [:].withDefault { new LinkedHashSet() }
+def cmo2uberon = [:].withDefault { new LinkedHashSet() }
+def cmo2pato = [:].withDefault { new LinkedHashSet() }
+def cmo2chebi = [:].withDefault { new LinkedHashSet() }
+
 
 cmolabels.each { cl, lab ->
     uberonlabels.each { k, v ->
-	if (lab.indexOf(v)>-1) {
+	v = Pattern.quote(v)
+	if (lab=~/(^|\s)${v}($|\s)/) {
 	    println "$lab\t$k\t$cl"
-	}
-    }
-    chebilabels.each { k, v ->
-	if (lab.indexOf(v)>-1) {
-	    println "$lab\t$k\t$cl"
+	    cmo2other[cl].add(k)
+	    cmo2uberon[cl].add(k)
 	}
     }
     patolabels.each { k, v ->
-	if (lab.indexOf(v)>-1) {
-	    println "$lab\t$k\t$cl"
+	v = Pattern.quote(v)
+	if (lab=~/(^|\s)${v}($|\s)/) {
+    	    println "$lab\t$k\t$cl"
+    	    cmo2other[cl].add(k)
+	    cmo2pato[cl].add(k)
+    	}
+    }
+    chebilabels.each { k, v ->
+	v = Pattern.quote(v)
+	if (lab=~/(^|\s)${v}($|\s)/) {
+    	    println "$lab\t$k\t$cl"
+    	    cmo2other[cl].add(k)
+	    cmo2chebi[cl].add(k)
+    	}
+    }
+}
+
+
+
+cmo.getClassesInSignature(true).each { k ->
+    if (cmo2uberon[k] != null) {
+	v = cmo2uberon[k]
+	v.each { cl ->
+	    manager.addAxiom(ont, subclass(k, some(r("has-part"), and(c("quality"), some(r("inheres-in-part-of"), cl)))))
+	}
+    }
+    if (cmo2uberon[k] != null && cmo2pato[k] != null) {
+	v1 = cmo2uberon[k]
+	v2 = cmo2pato[k]
+	v1.each { cl1 ->
+	    v2.each { cl2 ->
+		manager.addAxiom(ont, subclass(k, some(r("has-part"), and(cl2, some(r("inheres-in"), cl1)))))
+	    }
 	}
     }
 }
+
+
+
+manager.applyChange(new AddImport(ont, fac.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/mp.owl"))))
+manager.applyChange(new AddImport(ont, fac.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/pato.owl"))))
+manager.applyChange(new AddImport(ont, fac.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/uberon.owl"))))
+manager.applyChange(new AddImport(ont, fac.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/chebi.owl"))))
+manager.applyChange(new AddImport(ont, fac.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/cmo.owl"))))
+
+manager.saveOntology(ont, IRI.create(new File("qt.owl").toURI()))
